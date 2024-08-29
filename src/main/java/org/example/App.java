986fe0851f99c57e2c53ab229fce10568d960c70
yujihun1 <sk0253642@gmail.com>
2024-08-29 22:26:38 +0900

    6,7완

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -37,6 +37,31 @@ public class App {
                     System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
                 }
 
+            }else if(command.startsWith("삭제")){
+                String[] commandList = command.split("\\?",2);
+                String actionCode = commandList[0];
+                String[] paramsStr = commandList[1].split("=",2);
+
+                String key =paramsStr[0];
+                String value = paramsStr[1];
+                int idx= Integer.parseInt(value);
+
+                Article article =null;
+
+                for(int i=0; i<articleList.size();i++){
+                    if(articleList.get(i).getId()==idx){
+                        article =articleList.get(i);
+                    }
+                }
+
+                if(article==null){
+                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
+                }else{
+                    articleList.remove(article);
+                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
+                }
+
+
             }
         }
     }
